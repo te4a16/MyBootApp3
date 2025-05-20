01791705e1d310cc0202b4d32ce52dcbd6a52459
te4a16 <te220397@gmail.com>
2025-05-20 10:52:04 +0900

    課題5

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/DemoApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package jp.te4a.spring.boot.myapp5.mybootapp5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/HelloController.java
@@ -0,0 +1,30 @@
+package jp.te4a.spring.boot.myapp5.mybootapp5;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+* Hello world!
+*/
+@Controller
+public class HelloController
+{
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg","this is a setting message");
+        return "index";
+    }
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
+    }
+    @RequestMapping("/miyagi")
+    public String miyagi() {
+        return "Miyagi desu!";
+    }
+}
